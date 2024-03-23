0dcad005232b5815427bfdbf6d3150839e66d8b8
usernayeem <93907431+usernayeem@users.noreply.github.com>
2024-03-23 06:10:58 +0600

    Update heading  name

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,7 +5,7 @@ import { Typography } from '@mui/material'
 const Heading = () => {
   return (
     <>
-      <Typography variant="h1" sx={{ fontWeight: 900, fontSize: 25, textAlign: 'center', mt: 2 }}><span className={style.mania}>CRTPTO MANIA</span>
+      <Typography variant="h1" sx={{ fontWeight: 900, fontSize: 25, textAlign: 'center', mt: 2 }}><span className={style.mania}>CRTPTO Genius</span>
       </Typography>
     </>
   )
