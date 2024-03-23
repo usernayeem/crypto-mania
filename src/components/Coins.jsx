b386a260df935efb1b61d234f29b3a3102931422
usernayeem <93907431+usernayeem@users.noreply.github.com>
2024-03-23 23:24:03 +0600

    Add-home-page-skeleton

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import CoinRow from './CoinRow';
 import { Link } from 'react-router-dom';
 import Pagination from '@mui/material/Pagination';
+import { SkeletonComponent } from './Skeleton';
 
 // This component displays a list of coins with pagination.
 // It fetches the data from the CoinGecko API.
 export const Coins = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // The URL for fetching the data.
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
@@ -19,6 +21,7 @@ export const Coins = () => {
         const response = await fetch(url);
         const data = await response.json();
         setCoins(data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -52,20 +55,33 @@ export const Coins = () => {
         <p>Price</p>
         <p className="hidden">24h Change</p>
       </div>
-      {
-        // Map the coins in the current page to CoinRow components.
-        items.map((coinInfo) => (
-          <Link to={`/coin/${coinInfo.id}`} key={coinInfo.id}>
-            <CoinRow coinRow={coinInfo} />
-          </Link>
-        ))
-      }
-      <Pagination
-        className="pagination"
-        count={nPage}
-        color="primary"
-        onChange={(event, value) => setCurrentPage(value)}
-      />
+      {loading ? (
+        <div>
+          <SkeletonComponent />
+          <Pagination
+            className="pagination"
+            count={10}
+            color="primary"
+          />
+        </div>
+      ) : (
+        <div className='data'>
+          {
+            // Map the coins in the current page to CoinRow components.
+            items.map((coinInfo) => (
+              <Link to={`/coin/${coinInfo.id}`} key={coinInfo.id}>
+                <CoinRow coinRow={coinInfo} />
+              </Link>
+            ))
+          }
+          <Pagination
+            className="pagination"
+            count={nPage}
+            color="primary"
+            onChange={(event, value) => setCurrentPage(value)}
+          />
+        </div>
+      )}
     </>
   );
 };
